ee82c72091e9d6cba5653815600b4d700b18a945
Dmitry Vatutin <today.d89@gmail.com>
2020-03-23 22:41:54 +0300

    added java hometasks maven project

---
+++ java-hometasks/src/main/java/hometasks/App.java
@@ -0,0 +1,13 @@
+package hometasks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
