112c4d00113c8ab113ba0cc58c581fd23a2f0b67
Dmitry Vatutin <today.d89@gmail.com>
2020-05-26 16:24:09 +0300

    Added 20s to wait

--- java-hometasks/src/test/java/framework/page/AbstractPage.java
+++ java-hometasks/src/test/java/framework/page/AbstractPage.java
@@ -9,7 +9,7 @@ public abstract class AbstractPage
   protected WebDriver driver;
   protected final Logger logger = LogManager.getRootLogger();
 
-  protected final int WAIT_TIMEOUT_SECONDS = 40;
+  protected final int WAIT_TIMEOUT_SECONDS = 60;
 
   protected AbstractPage(WebDriver driver)
   {
