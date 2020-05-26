0f1e52770c23335347e239ce773d5eb32870ba76
Dmitry Vatutin <today.d89@gmail.com>
2020-05-26 16:05:03 +0300

    Added 10s to wait just to make sure. Also, Idea commit test

--- java-hometasks/src/test/java/framework/page/AbstractPage.java
+++ java-hometasks/src/test/java/framework/page/AbstractPage.java
@@ -9,7 +9,7 @@ public abstract class AbstractPage
   protected WebDriver driver;
   protected final Logger logger = LogManager.getRootLogger();
 
-  protected final int WAIT_TIMEOUT_SECONDS = 30;
+  protected final int WAIT_TIMEOUT_SECONDS = 40;
 
   protected AbstractPage(WebDriver driver)
   {
